43a48218a176b4a941fcceacdc64edb936923d47
Santiago Quiroz upegui <squirozu@unal.edu.co>
2020-12-19 16:13:24 -0500

    IIS

--- surgas-server/db.js
+++ surgas-server/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2');
 
 module.exports.pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'gases2copia',
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
