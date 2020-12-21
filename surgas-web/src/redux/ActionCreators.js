991afeaa23947c359f348aa314b9e44fb8f1c493
Santiago Quiroz upegui <squirozu@unal.edu.co>
2020-12-21 11:51:23 -0500

    logincomponet auth

--- surgas-web/src/redux/ActionCreators.js
+++ surgas-web/src/redux/ActionCreators.js
@@ -65,3 +65,110 @@ export const login = (user) => async (dispatch) => {
         dispatch(loginFailed(err));
     }
 }
+
+//Restore and change password 
+export const restoreRequest = () => ({
+    type: ActionTypes.RESTORE_REQUEST
+});
+
+export const restoreReset = () => ({
+    type: ActionTypes.RESTORE_RESET
+});
+
+export const restoreSuccess = (result) => ({
+    type: ActionTypes.RESTORE_SUCCESS,
+    payload: result
+});
+
+export const restoreFailed = (errmess) => ({
+    type: ActionTypes.RESTORE_FAILED,
+    payload: errmess
+});
+
+export const restorePassword = (user) => (dispatch) => {
+    dispatch(restoreRequest());
+
+    return fetch(baseBackUrl + 'users/forgot', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: "same-origin",
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, error => {
+            throw error;
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (response.success) {
+                dispatch(restoreSuccess(response));
+            } else {
+                var error = new Error('Error ' + response.status);
+                error.response = response;
+                throw error;
+            }
+        })
+        .catch(error => dispatch(restoreFailed(error)));
+}
+
+export const changePasswordRequest = () => ({
+    type: ActionTypes.CHANGEPASSWORD_REQUEST
+});
+
+export const changePasswordReset = () => ({
+    type: ActionTypes.CHANGEPASSWORD_RESET
+});
+
+export const changePasswordSuccess = (result) => ({
+    type: ActionTypes.CHANGEPASSWORD_SUCCESS,
+    payload: result
+});
+
+export const changePasswordFailed = (errmess) => ({
+    type: ActionTypes.CHANGEPASSWORD_FAILED,
+    payload: errmess
+});
+
+export const changePassword = (data) => (dispatch) => {
+    dispatch(changePasswordRequest());
+
+    return fetch(baseBackUrl + 'users/forgot/'+data.token, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: "same-origin",
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            if (response.status === 200) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, error => {
+            throw error;
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (response.status === 200) {
+                dispatch(changePasswordSuccess(response));
+            } else {
+                var error = new Error('Error ' + response.status);
+                error.response = response;
+                throw error;
+            }
+        })
+        .catch(error => dispatch(changePasswordFailed(error)));
+}
