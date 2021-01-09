69b3eb9777c216c3a9aacf7dcf11110ea87cde85
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-01-08 21:29:25 -0500

    grids magicos

--- surgas-web/src/components/ProductComponent.js
+++ surgas-web/src/components/ProductComponent.js
@@ -10,60 +10,110 @@ const ProductsComponent = () => {
             <h3>Productos :)</h3>
             <hr />
 
+            <div className="container">
+                <div className='card-container'>
+                    <Card>
+                        <CardImg top width="100%" src={baseFrontUrl + "public/productos/propano.PNG"} alt="propano" />
+                        <CardBody>
+                            <CardTitle tag="h5">Gas propano</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">Huele delicioso y cocina, el gas perfecto</CardSubtitle>
+                        </CardBody>
+                    </Card>
 
-            <CardDeck>
-                <Card>
-                    <CardImg top width="100%" src={baseFrontUrl + "public/productos/propano.PNG"} alt="propano" />
-                    <CardBody>
-                        <CardTitle tag="h5">Gas propano</CardTitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Huele delicioso y cocina, el gas perfecto</CardSubtitle>
-                    </CardBody>
-                </Card>
+                    <Card>
+                        <CardImg top width="100%" src={baseFrontUrl + "public/productos/acetileno.PNG"} alt="acetileno" />
+                        <CardBody>
+                            <CardTitle tag="h5">Acetileno</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">Hace bumbum, tal vez; probablemente tóxico</CardSubtitle>
+                        </CardBody>
+                    </Card>
 
-                <Card>
-                    <CardImg top width="100%" src={baseFrontUrl + "public/productos/acetileno.PNG"} alt="acetileno" />
-                    <CardBody>
-                        <CardTitle tag="h5">Acetileno</CardTitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Hace bumbum, tal vez; probablemente tóxico</CardSubtitle>
-                    </CardBody>
-                </Card>
+                    <Card>
+                        <CardImg top width="100%" src={baseFrontUrl + "public/productos/argon.PNG"} alt="argon" />
+                        <CardBody>
+                            <CardTitle tag="h5">Argón</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">El argón es la hostia</CardSubtitle>
+                        </CardBody>
+                    </Card>
 
-                <Card>
-                    <CardImg top width="100%" src={baseFrontUrl + "public/productos/argon.PNG"} alt="argon" />
-                    <CardBody>
-                        <CardTitle tag="h5">Argón</CardTitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">El argón es la hostia</CardSubtitle>
-                    </CardBody>
-                </Card>
+                    <Card>
+                        <CardImg top width="100%" src={baseFrontUrl + "public/productos/oxigeno.PNG"} alt="oxigeno" />
+                        <CardBody>
+                            <CardTitle tag="h5">Oxígeno</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">LO NECESITAS PARA RESPIRAR, CÓMPRALO YA!!</CardSubtitle>
+                        </CardBody>
+                    </Card>
 
-                <Card>
-                    <CardImg top width="100%" src={baseFrontUrl + "public/productos/oxigeno.PNG"} alt="oxigeno" />
-                    <CardBody>
-                        <CardTitle tag="h5">Oxígeno</CardTitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">LO NECESITAS PARA RESPIRAR, CÓMPRALO YA!!</CardSubtitle>
-                    </CardBody>
-                </Card>
+                    <Card>
+                        <CardImg top width="100%" src={baseFrontUrl + "public/productos/Co2.PNG"} alt="Co2" />
+                        <CardBody>
+                            <CardTitle tag="h5">Co2</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">Tú lo produces, y eres bastante bueno haciéndolo</CardSubtitle>
+                        </CardBody>
+                    </Card>
 
-                <Card>
-                    <CardImg top width="100%" src={baseFrontUrl + "public/productos/Co2.PNG"} alt="Co2" />
-                    <CardBody>
-                        <CardTitle tag="h5">Co2</CardTitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Tú lo produces, y eres bastante bueno haciéndolo</CardSubtitle>
-                    </CardBody>
-                </Card>
+                    <Card>
+                        <CardImg top width="100%" src={baseFrontUrl + "public/productos/extintores.PNG"} alt="extintores" />
+                        <CardBody>
+                            <CardTitle tag="h5">Extintores</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">Muy útil para apagar fuegos indeseados</CardSubtitle>
+                        </CardBody>
+                    </Card>
+                    <Card>
+                        <CardImg top width="100%" src={baseFrontUrl + "public/productos/propano.PNG"} alt="propano" />
+                        <CardBody>
+                            <CardTitle tag="h5">Gas propano</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">Huele delicioso y cocina, el gas perfecto</CardSubtitle>
+                        </CardBody>
+                    </Card>
 
-                <Card>
-                    <CardImg top width="100%" src={baseFrontUrl + "public/productos/extintores.PNG"} alt="extintores" />
-                    <CardBody>
-                        <CardTitle tag="h5">Extintores</CardTitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Muy útil para apagar fuegos indeseados</CardSubtitle>
-                    </CardBody>
-                </Card>
-                
-            </CardDeck>
+                    <Card>
+                        <CardImg top width="100%" src={baseFrontUrl + "public/productos/acetileno.PNG"} alt="acetileno" />
+                        <CardBody>
+                            <CardTitle tag="h5">Acetileno</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">Hace bumbum, tal vez; probablemente tóxico</CardSubtitle>
+                        </CardBody>
+                    </Card>
 
-            <br></br>
-            
+                    <Card>
+                        <CardImg top width="100%" src={baseFrontUrl + "public/productos/argon.PNG"} alt="argon" />
+                        <CardBody>
+                            <CardTitle tag="h5">Argón</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">El argón es la hostia</CardSubtitle>
+                        </CardBody>
+                    </Card>
+
+                    <Card>
+                        <CardImg top width="100%" src={baseFrontUrl + "public/productos/oxigeno.PNG"} alt="oxigeno" />
+                        <CardBody>
+                            <CardTitle tag="h5">Oxígeno</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">LO NECESITAS PARA RESPIRAR, CÓMPRALO YA!!</CardSubtitle>
+                        </CardBody>
+                    </Card>
+
+                    <Card>
+                        <CardImg top width="100%" src={baseFrontUrl + "public/productos/Co2.PNG"} alt="Co2" />
+                        <CardBody>
+                            <CardTitle tag="h5">Co2</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">Tú lo produces, y eres bastante bueno haciéndolo</CardSubtitle>
+                        </CardBody>
+                    </Card>
+
+                    <Card>
+                        <CardImg top width="100%" src={baseFrontUrl + "public/productos/extintores.PNG"} alt="extintores" />
+                        <CardBody>
+                            <CardTitle tag="h5">Extintores</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">Muy útil para apagar fuegos indeseados</CardSubtitle>
+                        </CardBody>
+                    </Card>
+                </div>
+
+
+
+                <br></br>
+            </div>
+
+
         </div>
     );
 };
