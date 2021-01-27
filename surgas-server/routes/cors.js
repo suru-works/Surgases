945e1e61d0a2beabc3e37ef6b564419fb2d8d2c9
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-01-27 13:57:35 -0500

    testapi funcional

--- surgas-server/routes/cors.js
+++ surgas-server/routes/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 var corsOptions = {
-    origin: 'localhost:3000',
+    origin: 'http://localhost:3000',
     methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
     credentials: true
 }
