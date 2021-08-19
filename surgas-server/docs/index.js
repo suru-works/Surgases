f018b7bb2e4a4693469449ddbee2546d60e43269
DanSBN <dansanchezbui@unal.edu.co>
2021-08-19 14:20:11 -0500

    Intento 4 de documentacion

--- surgas-server/docs/index.js
+++ surgas-server/docs/index.js
@@ -1,5 +1,7 @@
+const info = require('./info')
 const clientes = require('./clientes');
 
 module.exports = {
+    ...info,
     ...clientes
 };
---
+++ surgas-server/docs/info.js
@@ -0,0 +1,11 @@
+module.exports = {
+    openapi: '3.0.0',
+    info: {
+        title: 'Surgas API',
+        description: 'This is a REST API made with Express for the Surgas website.',
+        license: {
+            name: 'MIT License'
+        },
+        version: '1.0.0'
+    }
+};
