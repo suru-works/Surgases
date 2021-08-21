36b1f97f578412d1276f34b8099fd76b4d4ae207
DanSBN <dansanchezbui@unal.edu.co>
2021-08-21 10:23:55 -0500

    Revertido

--- surgas-server/utils.js
+++ surgas-server/utils.js
@@ -1,6 +1,6 @@
 module.exports.parseToJSON = (results) => {
     if (results.length > 0) {
-        return JSON.parse(JSON.stringify(results[0]));
+        return JSON.parse(JSON.stringify(results));
     } else {
         return []
     }
