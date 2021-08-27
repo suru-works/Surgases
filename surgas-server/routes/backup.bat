7c1f807c0776f446b1d7f5949ebc66a95693618c
DanSBN <dansanchezbui@unal.edu.co>
2021-08-27 18:30:51 -0500

    Pinches BATs

--- surgas-server/routes/backup.bat
+++ surgas-server/routes/backup.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\MariaDB 10.6\bin\mysqldump.exe" --user=%1 --password=%2 %3 > "Z:\Shared drives\suru-works\surgas\Respaldos\%date:~10,4%-%date:~7,2%-%date:~4,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%.sql"
+"C:\Program Files\MariaDB 10.6\bin\mysqldump.exe" --user=%1 --password=%2 %3 > "Z:\Shared drives\suru-works\surgas\Respaldos\%date:~11,4%-%date:~8,2%-%date:~5,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%.sql"
