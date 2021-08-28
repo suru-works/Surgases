092dc5e338f5114478bb02a32059f2ff21109742
DanSBN <dansanchezbui@unal.edu.co>
2021-08-28 16:44:23 -0500

    Limpieza en pagoRouter

--- surgas-server/routes/pagoRouter.js
+++ surgas-server/routes/pagoRouter.js
@@ -1,10 +1,13 @@
+const asyncHandler = require('express-async-handler');
+
 const db = require('../db');
 const auth = require('../auth');
-const asyncHandler = require('express-async-handler');
+const { parseToJSON } = require('../utils');
+
+const pool = db.pool;
 
 const pagoRouter = require('express').Router();
 pagoRouter.use(require('body-parser').json());
-const pool = db.pool;
 
 pagoRouter.route("/")
 .all((req, res, next) => {
@@ -12,7 +15,6 @@ pagoRouter.route("/")
     next();
 })
 .get(asyncHandler(async (req, res, next) => {
-    //const query = db.buildQuery('producto', req.query);
     let query = 'SELECT * FROM pago';
     const params = req.query;
     let conditions = [];
@@ -41,89 +43,51 @@ pagoRouter.route("/")
             values.push(params.montoMaximo);
         }
 
-        if (params.empleado) {
-            conditions.push('empleado LIKE ?');
-            values.push('%' + params.empleado + '%');
+        if (params.registrador) {
+            conditions.push('registrador LIKE ?');
+            values.push(`%${params.registrador}%`);
         }
 
-        if (params.usuario) {
-            conditions.push('usuario LIKE ?');
-            values.push('%' + params.usuario + '%');
+        if (params.beneficiario) {
+            conditions.push('beneficiario LIKE ?');
+            values.push(`%${params.beneficiario}%`);
         }
     }
 
-    const results = await pool.promise().execute(query + conditions.join(' AND '), values);
-    if (results) {
-        res.json(JSON.parse(JSON.stringify(results[0])))
-    } else {
-        throw {
-            status: 500
-        };
-    }
+    const [results,] = await pool.execute(query + conditions.join(' AND '), values);
+
+    res.json(parseToJSON(results));
 }))
-.post(auth.isAuthenticated, auth.isAdmin, asyncHandler(async (req, res, next) => {
-    pool.getConnection(async (err, conn) => {
-        const pago = req.body;
-        const result = await conn.promise().execute(
-            'INSERT INTO pago(fechaHora, monto, empleado, usuario) VALUES (?, ?, ?, ?)',
-            [pago.fechaHora, pago.monto, pago.empleado, req.user.username]
-        );
+.post(auth.isAuthenticated, auth.isEmployee, asyncHandler(async (req, res, next) => {
+    await pool.execute(
+        'INSERT INTO pago(fechaHora, monto, registrador, beneficiario) VALUES (?, ?, ?, ?)',
+        [pago.fechaHora, pago.monto, req.user.empleado, pago.beneficiario]
+    );
 
-        if (result[0].affectedRows == 1) {
-            conn.commit();
-            res.json(req.body);
-        } else {
-            conn.rollback();
-            throw {
-                status: 500
-            }
-        }
+    res.json({
+        success: true
     });
 }));
 
-pagoRouter.route("/:id")
+pagoRouter.route("/:codigo")
 .all((req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
     next();
 })
 .put(auth.isAuthenticated, auth.isAdmin, asyncHandler(async (req, res, next) => {
-    pool.getConnection(async (err, conn) => {
-        if (err) {
-            console.log(err);
-            next(err);
-        }
+    const query = db.buildUpdate('pago', { name: 'codigo', value: req.params.codigo }, req.body);
+    await pool.execute(query.query, query.values);
 
-        const query = db.buildUpdate('pago', { name: 'id', value: req.params.codigo }, req.body);
-        const result = await conn.promise().execute(query.query, query.values);
-        if (result[0].affectedRows == 1) {
-            conn.commit();
-            res.json({
-                msg: 'employee updated successfully'
-            });
-        } else {
-            conn.rollback();
-            next(new Error('update error'));
-        }
-    })
+    res.json({
+        success: true
+    });
 }))
 .delete(auth.isAuthenticated, auth.isAdmin, asyncHandler(async (req, res, next) => {
-    pool.getConnection(async (err, conn) => {
-        if (err) {
-            console.log(err);
-            next(err);
-        }
+    await pool.execute('DELETE FROM pago WHERE codigo = ?', [req.params.codigo]);
 
-        const result = await conn.promise().execute('DELETE FROM pago WHERE id = ?', [req.params.id]);
-        if (result[0].affectedRows == 1) {
-            conn.commit();
-            res.json({
-                msg: 'employee deleted successfully'
-            });
-        } else {
-            conn.rollback();
-            next(new Error('deletion error'));
-        }
-    })
+    res.json({
+        success: true
+    });
 }));
 
 module.exports = pagoRouter;
